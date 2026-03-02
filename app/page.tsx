a52551287b6dd72fdbf4e6b95bb92d29877cde89
junszhao <jun.zhao.ox@gmail.com>
2026-03-02 19:02:56 +0000

    front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Hero } from '@/components/hero'
 import { OrganizationIntro } from '@/components/organization-intro'
 import { StakeholderSection } from '@/components/stakeholder-section'
+import { MissionSection } from '@/components/mission-section'
+import { CHAIIntro } from '@/components/chai-framework'
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <Hero />
       <OrganizationIntro />
       <StakeholderSection />
+      <MissionSection />
+      <CHAIIntro />
     </main>
   )
 }
